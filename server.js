da2144a7614a69236a7231df099f392dde94e42c
iamkkaushik <160194586+iamkkaushik@users.noreply.github.com>
2024-11-16 16:52:48 +0530

    Update6 server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://bc-murex.vercel.app",
+    origin: "https://www.bhadracottage.in",
     methods: ["GET", "POST", "DELETE", "PUT"],
     // allowedHeaders: [
     //   "Content-Type",
